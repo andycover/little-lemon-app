b21362c710d145d2de588805926a8b150e87f306
A Anand <andycover@gmail.com>
2023-04-27 18:46:32 +0530

    Added the login screen, and some other small changes to feedback form

---
+++ components/LoginScreen.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import { ScrollView, Text, StyleSheet, TextInput } from "react-native";
+
+export default function LoginScreen() {
+    const [email, onChangeEmail] = useState('');
+    const [password, onChangePassword] = useState('');
+
+    return (
+        <ScrollView style={styles.container}>
+            <Text style={styles.headerText}>
+                Your favorite neighborhood bistro
+            </Text>
+            <Text style={styles.regularText}>
+                Login to continue
+            </Text>
+            <TextInput
+                style={styles.emailInput}
+                value={email}
+                onChangeText={onChangeEmail}
+                placeholder="you@email.com"
+                keyboardType="email-address"
+                maxLength={64}
+            />
+            <TextInput
+                style={styles.passwordInput}
+                value={password}
+                onChangeText={onChangePassword}
+                placeholder="password"
+                maxLength={16}
+            />
+        </ScrollView>
+    )
+}
+
+const styles = StyleSheet.create(
+    {
+        container: {
+            flex: 1,
+        },
+        headerText: {
+            padding: 40,
+            fontSize: 30,
+            color: '#EDEFEE',
+            textAlign: 'center',
+        },
+        regularText: {
+            fontSize: 24,
+            padding: 20,
+            marginVertical: 8,
+            color: '#EDEFEE',
+            textAlign: 'center',
+        },
+        emailInput: {
+            height: 40,
+            margin: 12,
+            borderWidth: 1,
+            padding: 10,
+            fontSize: 16,
+            borderColor: 'EDEFEE',
+            backgroundColor: '#F4CE14',
+        },
+        passwordInput: {
+            height: 40,
+            margin: 12,
+            borderWidth: 1,
+            padding: 10,
+            fontSize: 16,
+            backgroundColor: '#F4CE14',
+        },
+    }
+);
